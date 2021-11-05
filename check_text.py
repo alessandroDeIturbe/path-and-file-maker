6ae614d216be93c1d3890b77c2a7eb033d3cc983
Alessandro De Iturbe <63016931+alessandroDeIturbe@users.noreply.github.com>
2021-11-06 00:43:08 +0100

    Update check_text.py

--- check_text.py
+++ check_text.py
@@ -1,24 +1,49 @@
 import os
 
-wd = ''
+def quit(var):
+    if var.lower() == 'q':
+        print('\nBye!')
+        exit()
 
-while wd == '':
-    wd = input('Enter the path of the working directory: ')
+def chdir(work_dir):
+    wd = ''
+    while wd == '':
+        wd = work_dir
+        quit(wd)
 
-    if wd == '':
-        wd = os.getcwd()
+        if wd == '':
+            wd = os.getcwd()
 
-    if os.path.exists(wd):
-        print('exists')
-        os.chdir(wd)
-        print(os.getcwd())
+        if os.path.exists(wd):
+            os.chdir(wd)
 
+        else:
+            print('not exists')
+
+    print('The current directory is: ', os.getcwd())
+
+def check_text(file_name):
+    if os.path.isfile(file_name):
+        with open(file_name, 'r') as f:
+            text = f.read()
+            print(text)
+            quit(input('\nPress q to quit or any other key to continue: '))
     else:
-        print('not exists')
+        print('\nFile not found!')
+        quit(input('\nPress q to quit or any other key to continue: '))
 
-    pass
+chdir(input('\nEnter the working directory: '))
 
 with open("file_formats.txt", 'r') as f:
-    text = f.read().split(',')
+    extensions = f.read().split(',')
 
-print(text)
+name = ''
+names = [] 
+while name != 'done':
+    name = input('Enter the name of the file / folder: ')
+    quit(name)
+    if name == 'done' or name == '':
+        break
+    names.append(name)
+
+print('The names of the folders / files are:', names)
