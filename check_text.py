b6fb9a7525e172d8eea62eb43b8494a3da2aba40
Alessandro De Iturbe <63016931+alessandroDeIturbe@users.noreply.github.com>
2021-11-07 11:05:33 +0100

    Update check_text.py

--- check_text.py
+++ check_text.py
@@ -5,45 +5,60 @@ def quit(var):
         print('\nBye!')
         exit()
 
-def chdir(work_dir):
-    wd = ''
-    while wd == '':
-        wd = work_dir
-        quit(wd)
-
-        if wd == '':
-            wd = os.getcwd()
-
-        if os.path.exists(wd):
-            os.chdir(wd)
-
-        else:
-            print('not exists')
-
-    print('The current directory is: ', os.getcwd())
-
-def check_text(file_name):
-    if os.path.isfile(file_name):
-        with open(file_name, 'r') as f:
-            text = f.read()
-            print(text)
-            quit(input('\nPress q to quit or any other key to continue: '))
+def wd(work_dir):
+    if work_dir == '':
+        work_dir = os.getcwd()
+        os.chdir(work_dir)
+        print('\nWorking directory: ' + os.getcwd())
     else:
-        print('\nFile not found!')
-        quit(input('\nPress q to quit or any other key to continue: '))
+        while os.path.exists(work_dir) == False:
+            print('\nDirectory does not exist!')
+            work_dir = input('\nEnter a valid directory: ')
+            return False
+        
+        if os.path.exists(work_dir):
+            print('Exists:', os.path.exists(work_dir))
+            os.chdir(work_dir)
+            print('Current working directory:', os.getcwd())
+            return True
 
-chdir(input('\nEnter the working directory: '))
+def extensions():
+    with open(f"{os.path.abspath('file_formats.txt')}", 'r') as f:
+        extensions = f.read().split(',')
+        return extensions
 
-with open("file_formats.txt", 'r') as f:
-    extensions = f.read().split(',')
+def touch(file_name):
+    exists = os.path.exists(file_name)
+    extension = extensions()
+
+wd(input(f'>Enter the working directory (leave empty for choose this folder: {os.getcwd()}): '))
 
 name = ''
-names = [] 
-while name != 'done':
-    name = input('Enter the name of the file / folder: ')
-    quit(name)
+while name != 'done' or name != '':
+    name = input('\nEnter the file name (leave empty to quit): ')
+    if name == 'q':
+        quit(name)
     if name == 'done' or name == '':
         break
-    names.append(name)
-
-print('The names of the folders / files are:', names)
+    if name == '':
+        break
+    if os.path.exists(name):
+        print('\nFile already exists!')
+        continue
+    else:
+        print('\nFile does not exist!')
+        extension = input('\nEnter the file extension (leave empty to quit): ')
+        if extension == 'q':
+            quit(extension)
+        if extension == 'done':
+            break
+        if extension == '':
+            continue
+        if extension in extension:
+            print('\nFile extension is valid!')
+            with open(name, 'w') as f:
+                f.write('')
+            print('\nFile created!')
+        else:
+            print('\nFile extension is not valid!')
+            continue
