56f190fd5981fabe09fe9302b4e6e945a79fdc66
alessandrodeiturbe <aledei2250@gmail.com>
2023-07-04 15:25:01 +0200

    Fix bugs

--- pfm/pfm.py
+++ pfm/pfm.py
@@ -6,49 +6,46 @@ from colorama import Fore, Back, Style
 
 
 def error(message):
-   # Formatta un messaggio di errore con sfondo rosso e testo bianco.
+    # Format an error message with red background and white text.
     return f'{Back.RED}{Fore.WHITE}{message}{Style.RESET_ALL}'
 
 
 def message(message):
-   # Formatta un messaggio con sfondo blu e testo bianco.
+    # Format a message with blue background and white text.
     return f'{Back.BLUE}{Fore.WHITE}{message}{Style.RESET_ALL}'
 
 
 def create_file(path):
-   # Crea un file nel percorso specificato.
+    # Create a file at the specified path.
     try:
         with open(path, 'w') as f:
             f.write('')
-        print(f'File "{path}" creato con successo.')
+        print(f'File "{path}" created successfully.')
     except Exception as e:
-        print(
-            error(f'Errore durante la creazione del file "{path}": {str(e)}'))
+        print(error(f'Error creating file "{path}": {str(e)}'))
 
 
 def create_directory(path):
-   # Crea una cartella nel percorso specificato.
+    # Create a directory at the specified path.
     try:
         os.mkdir(path)
-        print(f'Cartella "{path}" creata con successo.')
+        print(f'Directory "{path}" created successfully.')
     except Exception as e:
-        print(
-            error(f'Errore durante la creazione della cartella "{path}": {str(e)}'))
+        print(error(f'Error creating directory "{path}": {str(e)}'))
 
 
 def parse_arguments():
-   # Analizza gli argomenti della riga di comando.
+    # Parse command line arguments.
     parser = argparse.ArgumentParser(
-        description='Crea file o cartelle.')
-    parser.add_argument(
-        'path', help='Percorso del file o della cartella da creare')
+        description='Create files or directories.')
+    parser.add_argument('path', help='Path of the file or directory to create')
     parser.add_argument('--start-folder', '-s',
-                        help='Percorso della cartella di partenza (opzionale)')
+                        help='Starting folder path (optional)')
     return parser.parse_args()
 
 
 def main():
-   # Funzione principale per gestire la creazione di file o cartelle.
+    # Main function to handle the creation of files or directories.
     args = parse_arguments()
     path = args.path
     start_folder = args.start_folder
@@ -58,28 +55,29 @@ def main():
             os.chdir(start_folder)
         except Exception as e:
             print(
-                error(f'Errore durante il cambio della directory a "{start_folder}": {str(e)}'))
-            if input('Continuare nella cartella corrente? y/[N]').lower() != 'y':
+                error(f'Error changing directory to "{start_folder}": {str(e)}'))
+            if input('Continue in the current folder? y/[N]').lower() != 'y':
                 return
 
     if ',' in path:
-        # Gestisci la creazione di file e cartelle nella stessa directory.
+        # Handle creation of files and directories in the same directory.
         segments = path.split(',')
         base_dir = os.path.dirname(segments[0])
         os.makedirs(base_dir, exist_ok=True)
         os.chdir(base_dir)
         for segment in segments:
-            if os.path.isfile(segment):
-                print(error(f'Errore: "{segment}" è già un file esistente.'))
+            file_name, file_ext = os.path.splitext(segment)
+            if file_ext:
+                create_file(segment)
             else:
                 create_directory(segment)
     else:
         if os.path.exists(path):
-            print(error(f'Errore: "{path}" esiste già.'))
+            print(error(f'Error: "{path}" already exists.'))
             return
 
         if os.path.isfile(path):
-            print(error(f'Errore: "{path}" è già un file esistente.'))
+            print(error(f'Error: "{path}" is already an existing file.'))
         else:
             create_file(path)
 
