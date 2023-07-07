cbc64cd6e92a85f583d26cc95e17c1436d04d497
alessandrodeiturbe <aledei2250@gmail.com>
2023-07-07 21:14:38 +0200

    updated .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-                name = 'pfm',
-                version = '1.2.0',
-                description = 'pfm is a command-line tool that allows you to create files and directories in an easy and efficient way. It provides a simple interface to create files and directories with customizable names and locations.',
-                packages = find_packages(),
-                entry_points = {
-                        'console_scripts': [
-                                'pfm = pfm.pfm:main',
-                                ],
-                        },
-                install_requires = requirements,
-                )
+    name='pfm',
+    version='1.2.0',
+    description='pfm is a command-line tool that allows you to create files and directories in an easy and efficient way. It provides a simple interface to create files and directories with customizable names and locations.',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pfm = pfm.pfm:main',
+        ],
+    },
+    install_requires=requirements,
+)
